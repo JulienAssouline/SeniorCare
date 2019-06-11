194f5a65ae588c49036216e6d1bc92a5fa8f3735
JulienAssouline <julien1993@hotmail.ca>
2019-06-11 17:49:06 -0400

    search_styles_rating

--- client/App/Components/Search/SearchScreen.js
+++ client/App/Components/Search/SearchScreen.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollView, Text, View } from 'react-native'
 import { Button } from '@ant-design/react-native';
 import { useQuery } from 'react-apollo-hooks';
 import gql from "graphql-tag";
+import styles from "../Styles/searchStyles/searchStyles"
+import StarRating from 'react-native-star-rating';
+import Ratings from "./Ratings"
+
 
 
 const GET_CAREGIVERS = gql`
@@ -24,27 +28,46 @@ const GET_CAREGIVERS = gql`
 
 const SearchScreen = () => {
 
+  const [starCount, setStarCount] = useState(0)
+
   const {data, error, loading} = useQuery(GET_CAREGIVERS)
 
   if (data.getCaregiver === undefined) { return (<Text> ...loading </Text>)}
 
-  console.log(data.getCaregiver)
+  const currentYear = new Date().getFullYear()
+
+  data.getCaregiver.forEach((d,i) => {
+    const year = d.birthdate.split("-")[0]
+    d.Age = currentYear - year
+  })
+
+  function onStarRatingPress(rating) {
+    setStarCount(rating)
+  }
 
   return (
     <ScrollView>
+    <View style = {styles.MainContainer}>
       {
         data.getCaregiver.map((d,i) => (
-          <View key = {i}>
-            <Text> {d.fullname} </Text>
-            <Text> {d.location} </Text>
-            <Text> {d.average_rating} </Text>
-            <Text> {d.years_experience} </Text>
-            <Text> {d.hourly_rate} </Text>
-            <Text> {d.hourly_rate} </Text>
+          <View style = {styles.searchContainer} key = {i}>
+            <Text style = {styles.fullName}> {d.fullname} </Text>
+            <View style = {styles.ratingLocationContainer}>
+              <Ratings data = {d.average_rating} />
+              <Text style = {styles.ratingText}> {d.average_rating} </Text>
+              <Text style = {styles.locationText}> | </Text>
+              <Text style = {styles.locationText}> {d.location} </Text>
+            </View>
+            <View style = {styles.experienceRateContainer}>
+              <Text style = {styles.backgroundInfoText}> {`${d.years_experience} years experience`} </Text>
+              <Text style = {styles.backgroundInfoText}> {`From $${d.hourly_rate / 100}/hour`} </Text>
+            </View>
+            <Text style = {styles.backgroundInfoText}> {`${d.Age} years old`} </Text>
           </View>
 
           ))
       }
+    </View>
     </ScrollView>
   )
 }
