1235742c0eeec189a00969859c242923bb7ed745
Mark Olech <markolech@Marks-MacBook-Pro.local>
2019-06-12 15:27:58 -0400

    Completed bottom stack and added Links

---
+++ client/App/Navigation/AppStackNavigator.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+// THIS WILL NEED TO CHANGE BASED ON NEW SCREENS AND LOGIN USING KOGNITO
+// THIS FILE IS CURRENTLY NOT BEING USED
+const AppStackNavigator = StackNavigator({
+    loginFlow: { 
+      screen: StackNavigator({
+        splash: { screen: SplashScreen },
+        login: { screen: LoginScreen },
+      })
+    },
+    mainFlow: {
+      screen: StackNavigator({
+        main: { screen: MainScreen },
+        settings: { screen: SettingsScreen },
+        someTab: { 
+          screen: TabNavigator({
+            firstTab: { screen: FirstTabScreen },
+            secondTab: { screen: SecondTabScreen },
+            thirdTab: { screen: ThirdTabScreen }
+          })
+        }
+      })
+    }
+  });
+
+  export default AppStackNavigator;
+
