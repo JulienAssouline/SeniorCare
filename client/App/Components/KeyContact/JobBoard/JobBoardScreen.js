19767c0ac95e9700eaeaced3d1be4b31d4d4dc68
jimmiekudo <jeffalanjohnson22@gmail.com>
2019-06-18 09:36:39 -0400

    archive page has buttons that lead to other pages

--- client/App/Components/KeyContact/JobBoard/JobBoardScreen.js
+++ client/App/Components/KeyContact/JobBoard/JobBoardScreen.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import { ScrollView, Text, TouchableOpacity } from 'react-native'
+import React, {useEffect} from 'react'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { Button } from 'react-native-elements'
 import styles from '../../Styles/JobDashboardScreen/JobDashboardScreenStyle'
+import gql from 'graphql-tag'
+import { useQuery } from 'react-apollo-hooks'
 
-const JobBoardScreen = props => {
+
+const JobBoardScreen = (props) => {
+  
+  
+
   const handleGoToSeniorDetails = async () => {
     props.navigation.navigate('Overview')
   }
 
+
   return (
     <ScrollView style={styles.MainContainer}>
+
       <Text style={styles.ExempleText}>This is JobDashboardScreen!</Text>
       <Button
         title='Go to: Post a Job'
@@ -17,14 +25,18 @@ const JobBoardScreen = props => {
         style={styles.ExempleButton}
         onPress={handleGoToSeniorDetails}
       />
+      
 
-
-      <TouchableOpacity
+      <TouchableOpacity>   
+      <Button
+        title='Archive'
+        type='primary'
+        style={styles.ExempleButton}
         onPress={() =>
           props.navigation.navigate("Archive")}
-      >
-        <Text>Archive</Text>
+      />
       </TouchableOpacity>
+ 
     </ScrollView>
   )
 }
