c21810f76a4ca93c7c38b1eb797dc6fdf1625401
JulienAssouline <julien1993@hotmail.ca>
2019-06-20 11:52:43 -0400

    conversation screen

--- client/App/Components/Messages/MessagesScreen.js
+++ client/App/Components/Messages/MessagesScreen.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { ScrollView, Text } from 'react-native'
-import { Button } from 'react-native-elements'
+import { ScrollView, Text, View } from 'react-native'
 import styles from '../Styles/Messages/MessagesStyles'
+import { useQuery, useMutation } from 'react-apollo-hooks'
+import {GET_CAREGIVER_CONVO} from "../../graphql-queries/queries"
+import { Avatar, Button } from 'react-native-elements'
+import Icon from "react-native-vector-icons/Ionicons";
+import { List, ListItem } from 'react-native-elements'
 
-const MessagesScreen = () => {
+
+
+const MessagesScreen = (props) => {
+  const conversation_id = props.navigation.getParam('conversation_id');
   return (
-    <ScrollView style={styles.MainContainer}>
-      <Text style={styles.ExempleText}>This is MessagesScreen!</Text>
-      <Button title='I am cool' type="solid" style={styles.ExempleButton} />
+    <ScrollView>
+      <View style = {styles.MainContainer}>
+        <Text> {"Conversation ID:" + conversation_id} </Text>
+      </View>
     </ScrollView>
   )
 }
