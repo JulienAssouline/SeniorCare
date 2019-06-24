fb72489d5d6c82fd09fd55d53c3cba3dee19f7e9
jimmiekudo <jeffalanjohnson22@gmail.com>
2019-06-24 15:42:31 -0400

    fixed Schema

--- client/App/Components/KeyContact/JobBoard/JobBoardJobs.js
+++ client/App/Components/KeyContact/JobBoard/JobBoardJobs.js
@@ -1,82 +1,72 @@
-import React, {useEffect, useState} from 'react'
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
-import { Button } from 'react-native-elements'
-import styles from '../../Styles/JobDashboardScreen/JobDashboardScreenStyle'
-import gql from 'graphql-tag'
-import { useQuery } from 'react-apollo-hooks'
-import Loading from '../../Loading/Loading'
+// import React, {useEffect, useState} from 'react'
+// import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+// import { Button } from 'react-native-elements'
+// import styles from '../../Styles/JobDashboardScreen/JobDashboardScreenStyle'
+// import gql from 'graphql-tag'
+// import { useQuery } from 'react-apollo-hooks'
+// import Loading from '../../Loading/Loading'
 
-const ARCHIVED_JOBS = gql `
-  query{
-    ArchivedJobs{
-      id
-      title
-      date_created
-      start_date
-      hourly_rate
-    }
-  }
-`
+// const ARCHIVED_JOBS = gql `
+//   query{
+//     ArchivedJobs{
+//       id
+//       title
+//       date_created
+//       start_date
+//       hourly_rate
+//     }
+//   }
+// `
 
-const JobBoardJobs = (props) => {
+// const JobBoardJobs = (props) => {
+
+//   const [jobs, setJobs] = useState({});
+
+//   const { data, error, loading } = useQuery(ARCHIVED_JOBS);
+//   console.log('heree my data',data)
+
+//   if(loading){
+//     return <Loading/>
+//   };
+//   if (error){
+//     return <Text>Error!</Text>
+//   }
+
+//     return(
+//       <ScrollView>
+//         {data.ArchivedJobs.map(elem => (
+
+            
+//          <ScrollView> 
+//           <View style={styles.MainView}>
+
+//           <View>
+//           {console.log('all my datess',elem.date)}
+// //             <Text style={styles.DateText}> Posted {dateCreated}</Text>
+// //             <Text onPress={ () => onclick(elem.id)}>...</Text>
+// //             <Text key = {elem.id} style={styles.JobText}> {elem.title}</Text> 
+// //           </View>
+
+// //           <View style={styles.JobInfo}>
+// //             <Text style={{fontSize: 16}}> Starts {startDate}</Text>
+// //             <Text style={{fontSize: 16}}> ${elem.hourly_rate}/hr</Text>  
+// //           </View>
+ 
+//           </View>
+//           </ScrollView> 
+//         ))}
 
 
-  const [jobs, setJobs] = useState({});
-
-  const { data, error, loading } = useQuery(ARCHIVED_JOBS);
+//       </ScrollView>
+//     )
+// }
   
 
-  if(loading){
-    return <Loading/>
-  };
-  if (error){
-    return <Text>Error!</Text>
-  }
-  return (
-
-    <ScrollView style={styles.MainContainer}>
-
-{data.ArchivedJobs.map(elem =>{
-
-let date = new Date(parseInt(elem.date_created));
-let options = {
-  month: 'long', year: 'numeric', day: 'numeric',
-  };
-let dateCreated = date.toLocaleDateString('en', options);
-
-let newDate = new Date(parseInt(elem.start_date));
-let startDate = newDate.toLocaleDateString('en',options)
-
-return(
-<ScrollView>
 
 
-<View style={styles.MainView}>
-<View >
-<View>
-  <Text style={styles.DateText}> Posted {dateCreated}</Text>
-  <Text onPress={ () => onclick(elem.id)}>...</Text>
-  <Text key = {elem.id} style={styles.JobText}> {elem.title}</Text> 
+  
+    
 
-</View>
-
-<View style={styles.JobInfo}>
-  <Text style={{fontSize: 16}}> Starts {startDate}</Text>
-  <Text style={{fontSize: 16}}> ${elem.hourly_rate}/hr</Text>  
-</View>
-
-</View>
-
-</View>
-</ScrollView>
-
-)
-
-})}
-    </ScrollView>
-  )
-}
-
-export default JobBoardJobs
+// export default JobBoardJobs
 
 
