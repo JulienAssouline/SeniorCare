3ad091c327ee4239ce531cbeb2f1ad7c7492c0a6
alam1386 <alam1386@gmail.com>
2019-06-25 13:29:13 -0400

    The basic edit page

---
+++ client/App/Components/Styles/Profile/Edit.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+  Profile: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 25,
+  },
+  ProfileName: {
+    fontSize: 20,
+    padding: 15,
+    fontWeight: '600',
+  },
+  ProfileButton: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderWidth: 2,
+    borderBottomColor: '#eee',
+    borderColor: '#fff',
+    borderStyle: 'solid',
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  ProfileButtonForgot: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderWidth: 2,
+    borderBottomColor: '#eee',
+    borderColor: '#fff',
+    borderStyle: 'solid',
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  ProfileButtonText: {
+    fontSize: 16,
+    padding: 15,
+  },
+  ProfileButtonIcon: {
+    fontSize: 16,
+    padding: 15,
+    color: '#3F7DFB'
+  }
+})
+
+export default styles
