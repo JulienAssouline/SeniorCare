53f27f5d97ba0b72e7402b9763261125dfcc8ae5
vdumouchel <vudmouchel@me.com>
2019-06-27 10:35:27 -0400

    fixed the profile S3 Upload in Profile and added background curves to Caregiver jobs and find screens<

--- client/App/Components/Caregiver/Find/Find.js
+++ client/App/Components/Caregiver/Find/Find.js
@@ -1,68 +1,76 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 
-import { ScrollView, Text, View, TouchableOpacity } from 'react-native'
-import { Avatar, Button, Card, ListItem } from 'react-native-elements'
-
+import { ScrollView, Text, View, TouchableOpacity, Image } from 'react-native'
+import { Button } from 'react-native-elements'
 import { useQuery } from 'react-apollo-hooks'
 import { GET_BASIC_JOB_POSTING } from '../../../graphql-queries/queries'
 import { connect } from 'react-redux'
-
 import { backgroundStyles } from '../../Styles/GeneralStyles'
 import checkCognitoSession from "../../utils/checkCognitoSession"
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 import JobPosting from '../JobPosting'
 
+
 const mapStateToProps = state => {
-  return {
-    key_contact_id: state.user_id
-  }
+	return {
+		key_contact_id: state.user_id
+	}
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    onKeyContactIdUpdate: (value) => dispatch({ type: 'KEYCONTACTID', payload: value })
-  }
+	return {
+		onKeyContactIdUpdate: (value) => dispatch({ type: 'KEYCONTACTID', payload: value })
+	}
 }
+
+const yellowCurve = require('../../../Images/WelcomeScreen/yellow-curve.png')
 
 const Find = props => {
 	const { data, error, loading } = useQuery(GET_BASIC_JOB_POSTING)
 
 	const handleGoJobInformation = (id) => {
-		props.navigation.navigate('CaregiverApplyToJob', {id: id})
-	}	
-	
+		props.navigation.navigate('CaregiverApplyToJob', { id: id })
+	}
+
 	useEffect(
-	  () => {
-	    checkCognitoSession(props)
-	  },
-	[])
+		() => {
+			checkCognitoSession(props)
+		},
+		[])
 
 	if (loading) return <View><Text>loading</Text></View>
 
 	if (error) return <View><Text>Error</Text></View>
 
 	return (
-		<ScrollView style={backgroundStyles.background}>
-			<Button
-				title='go to keycontact stack'
-				onPress={() => props.navigation.navigate('Overview')}
+		<View style={{ flex: 1, backgroundColor: '#EEF5FB' }}>
+			<Image
+				source={yellowCurve}
+				style={{ height: hp(44), width: wp(100), position: 'absolute', bottom: 0, padding: 0, margin: 0, backgroundColor: 'transparent' }}
 			/>
-			<View>
-				{data.getJobPosts.map(jobPost => (
-					<TouchableOpacity 
-						key={jobPost.id} 
-						onPress={() => handleGoJobInformation(jobPost.id)}
-					>
-						<JobPosting
-							keyContact={jobPost.getKeyContact}
-							dateCreated={jobPost.date_created}
-							basicInformation={jobPost.getBasicInformation}
-							serviceDetails={jobPost.getServiceDetails}
-						/>
-					</TouchableOpacity>
-				))}
-			</View>
-		</ScrollView>
+			<ScrollView>
+				<Button
+					title='go to keycontact stack'
+					onPress={() => props.navigation.navigate('Overview')}
+				/>
+				<View>
+					{data.getJobPosts.map(jobPost => (
+						<TouchableOpacity
+							key={jobPost.id}
+							onPress={() => handleGoJobInformation(jobPost.id)}
+						>
+							<JobPosting
+								keyContact={jobPost.getKeyContact}
+								dateCreated={jobPost.date_created}
+								basicInformation={jobPost.getBasicInformation}
+								serviceDetails={jobPost.getServiceDetails}
+							/>
+						</TouchableOpacity>
+					))}
+				</View>
+			</ScrollView>
+		</View>
 	)
 }
 
