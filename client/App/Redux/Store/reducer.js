3598e0da2b5b2a9de41baa995f87ca6f37fad0d8
jimmiekudo <jeffalanjohnson22@gmail.com>
2019-06-27 14:15:26 -0400

    Commented out the seed data from Redux

--- client/App/Redux/Store/reducer.js
+++ client/App/Redux/Store/reducer.js
@@ -2,58 +2,58 @@ const initialState = {
 
 	user_id: '',
 
-	postJob: {
-		position: {
-			formPosition: 0,
-			overviewPosition: 0,
-			completedSections: [0, 1, 2, 3, 4]
-		},
-		serviceDetails: {
-			appointments: false,
-			bathing: false,
-			companionship: false,
-			dressing: false,
-			driving: false,
-			errands: true,
-			feeding: true,
-			grooming: true,
-			housekeeping: false,
-			laundry: false,
-			mealPrep: false,
-			mobility: false,
-			shopping: false,
-		},
-		basicInformation: {
-			title: 'Finding Nemo',
-			startDate: '2003-05-30 11:11:30-07',
-			endDate: '2003-06-18 20:15:40-07',
-			address: '42 Wallaby Way',
-			city: 'Sydney',
-			province: 'Austrailia?',
-			postalCode: 'A1A1A1',
-			rate: 20,
-		},
-		seniorDetails: {
-			seniorName: 'Marlin',
-			gender: 'MALE',
-			birthdate: '2000-06-18 03:22:01-07',
-			relationship: 'son',
-			bio: 'a clownfish',
-			medicalCondition: 'sharkphobia',
-			language: 'fish',
-		},
-		houseDetails: {
-			cigSmoking: false,
-			pets: true,
-			cannabis: false
-		},
-		caregiverPreferences: {
-			availability: 'LIVEIN',
-			preferredGender: 'MALE',
-			validDriverLicense: true,
-		},
-	}
-}
+// 	postJob: {
+// 		position: {
+// 			formPosition: 0,
+// 			overviewPosition: 0,
+// 			completedSections: [0, 1, 2, 3, 4]
+// 		},
+// 		serviceDetails: {
+// 			appointments: false,
+// 			bathing: false,
+// 			companionship: false,
+// 			dressing: false,
+// 			driving: false,
+// 			errands: true,
+// 			feeding: true,
+// 			grooming: true,
+// 			housekeeping: false,
+// 			laundry: false,
+// 			mealPrep: false,
+// 			mobility: false,
+// 			shopping: false,
+// 		},
+// 		basicInformation: {
+// 			title: 'Finding Nemo',
+// 			startDate: '2003-05-30 11:11:30-07',
+// 			endDate: '2003-06-18 20:15:40-07',
+// 			address: '42 Wallaby Way',
+// 			city: 'Sydney',
+// 			province: 'Austrailia?',
+// 			postalCode: 'A1A1A1',
+// 			rate: 20,
+// 		},
+// 		seniorDetails: {
+// 			seniorName: 'Marlin',
+// 			gender: 'MALE',
+// 			birthdate: '2000-06-18 03:22:01-07',
+// 			relationship: 'son',
+// 			bio: 'a clownfish',
+// 			medicalCondition: 'sharkphobia',
+// 			language: 'fish',
+// 		},
+// 		houseDetails: {
+// 			cigSmoking: false,
+// 			pets: true,
+// 			cannabis: false
+// 		},
+// 		caregiverPreferences: {
+// 			availability: 'LIVEIN',
+// 			preferredGender: 'MALE',
+// 			validDriverLicense: true,
+// 		},
+// 	}
+// }
 
 const reducer = (state = initialState, action )  => {
 	console.log('REDUCER', state, action)
