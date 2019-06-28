23ce9b5deaa46ef0486a4d75715199054cf7dca2
vdumouchel <vudmouchel@me.com>
2019-06-28 08:32:09 -0400

    fixed avatar in caregiver convo and fixed CSS + added curve to to Keycontact Jobs

--- client/App/Components/KeyContact/JobBoard/JobBoardJobs.js
+++ client/App/Components/KeyContact/JobBoard/JobBoardJobs.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, Text, TouchableOpacity, View, Image } from 'react-native'
 import { Button, Card } from 'react-native-elements'
 import styles from '../../Styles/JobDashboardScreen/JobDashboardScreenStyle'
 import gql from 'graphql-tag'
 import { useQuery } from 'react-apollo-hooks'
 import Loading from '../../Loading/Loading'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import checkCognitoSession from '../../utils/checkCognitoSession'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 // const ARCHIVED_JOBS = gql`
 //   query{
@@ -19,11 +20,11 @@ import checkCognitoSession from '../../utils/checkCognitoSession'
 //   }
 // `
 
-const mapStateToProps =  state => {
+const mapStateToProps = state => {
 
 
   const user_id = state.user_id
-  return{
+  return {
     user_id: user_id
   }
 }
@@ -33,6 +34,9 @@ const mapDispatchToProps = dispatch => {
     onKeyContactIdUpdate: (value) => dispatch({ type: 'KEYCONTACTID', payload: value })
   }
 }
+
+
+const blueCurve = require('../../../Images/WelcomeScreen/blue-curve.png')
 
 const KEY_CONTACT_JOBS = gql`
 query getKeyContactJobPosts($id:ID!) {
@@ -61,62 +65,69 @@ const JobBoardJobs = (props) => {
   let user_id = props.user_id
 
   const { data, error, loading } = useQuery(KEY_CONTACT_JOBS, {
-    variables: {id: user_id}
+    variables: { id: user_id }
   })
-console.log('here is my data',data)
-  if(loading) return <Loading/>
+  console.log('here is my data', data)
+  if (loading) return <Loading />
 
-  if(error) {
+  if (error) {
 
-    return <Loading/>
+    return <Loading />
   }
-  
+
   return (
+    <View style={{ flex: 1, backgroundColor: '#EEF5FB' }}>
+      <Image
+        source={blueCurve}
+        style={{ height: hp(35), width: wp(100), position: 'absolute', bottom: 0, padding: 0, margin: 0, backgroundColor: 'transparent' }}
+      />
+      <ScrollView style={{ flex: 1, backgroundColor: 'transparent' }}>
 
-    <ScrollView style={styles.MainContainer}>
+        {data.getKeyContactJobPosts.map((elem, index) => {
 
-      {data.getKeyContactJobPosts.map((elem, index) => {
+          let date = new Date(parseInt(elem.date_created));
+          let options = {
+            month: 'long', year: 'numeric', day: 'numeric',
+          };
+          let dateCreated = date.toLocaleDateString('en', options);
 
-        let date = new Date(parseInt(elem.date_created));
-        let options = {
-          month: 'long', year: 'numeric', day: 'numeric',
-        };
-        let dateCreated = date.toLocaleDateString('en', options);
+          let newDate = new Date(parseInt(elem.start_date));
+          let startDate = newDate.toLocaleDateString('en', options)
 
-        let newDate = new Date(parseInt(elem.start_date));
-        let startDate = newDate.toLocaleDateString('en', options)
+          return (
 
-        return (
+            <ScrollView key={index}>
+              <View style={styles.CutCard}>
+                <View style={styles.Card}>
+                  <View>
+                    <Text style={styles.DateText}> Posted {dateCreated}</Text>
+                    <Text key={elem.id} style={styles.JobText}> {elem.title}</Text>
+                  </View>
 
-          <ScrollView key={index}>
-            <View style={styles.CutCard}>
-                <View style = {styles.Card}>
-                    <View>
-                      <Text style={styles.DateText}> Posted {dateCreated}</Text>
-                      <Text key={elem.id} style={styles.JobText}> {elem.title}</Text>
-                    </View>
+                  <View style={styles.JobInfo}>
+                    <Text>{elem.fullname}</Text>
+                    <Text style={{ fontSize: 14 }}> Starts {startDate}</Text>
+                  </View>
+                  <View>
+                    <Text style={{ fontSize: 16, color: '#3F7DFB', fontWeight: 600 }}> ${elem.hourly_rate}.00 / hour</Text>
+                  </View>
+                </View>
 
-                    <View style={styles.JobInfo}>
-                      <Text>{elem.fullname}</Text>
-                      <Text style={{ fontSize: 14 }}> Starts {startDate}</Text>
-                      <Text style={{ fontSize: 14 }}> ${elem.hourly_rate}/hr</Text>
-                    </View>
-              </View>
-
-              <View >
-                <View style={styles.Applicants}>
-                  <Text style={{color:'white', fontSize:45,paddingTop:20}}>{elem.applicants.length}</Text>
-                  <Text style={styles.JobText2}>Applicants</Text>
+                <View >
+                  <View style={styles.Applicants}>
+                    <Text style={{ color: 'white', fontSize: 45, paddingTop: 20 }}>{elem.applicants.length}</Text>
+                    <Text style={styles.JobText2}>Applicants</Text>
+                  </View>
                 </View>
               </View>
-            </View>
 
-          </ScrollView>
+            </ScrollView>
 
-        )
+          )
 
-      })}
-    </ScrollView>
+        })}
+      </ScrollView>
+    </View>
   )
 
 }
--- client/App/Components/Styles/JobDashboardScreen/JobDashboardScreenStyle.js
+++ client/App/Components/Styles/JobDashboardScreen/JobDashboardScreenStyle.js
@@ -5,7 +5,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#E9F6FF',
   },
-  MainView:{
+  MainView: {
     flex: 1,
     flexDirection: 'row',
     backgroundColor: "#FFFFFF",
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
   ExempleButton: {
     backgroundColor: '#569CF2',
   },
-  bluebox:{
+  bluebox: {
     backgroundColor: '#244397',
     position: 'absolute',
     // left:     100,
@@ -30,28 +30,30 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     backgroundColor: "white",
     marginLeft: 15,
-
-    },
-  CutCard:{
+    width: 240,
+    marginRight: 8,
+    height: 140,
+  },
+  CutCard: {
     flex: 1,
     flexDirection: 'row',
-    shadowOffset:{  width: 1,  height: 1,  },
-shadowColor: '#CCCCCC',
-shadowOpacity: 1.0,
+    shadowOffset: { width: 1, height: 1, },
+    shadowColor: '#CCCCCC',
+    shadowOpacity: 1.0,
   },
-  Applicants:{
+  Applicants: {
     marginTop: 20,
     marginBottom: 20,
-    backgroundColor:"#3633ff",
+    backgroundColor: "#244397",
     height: 140,
-    marginLeft:5,
+    marginLeft: 5,
     padding: 10,
     marginRight: 0,
     justifyContent: "center",
     alignItems: "center"
 
-  }, 
-   AppText:{
+  },
+  AppText: {
     fontFamily: 'SFProText-Bold',
     fontSize: 14,
     color: 'blue',
@@ -61,26 +63,26 @@ shadowOpacity: 1.0,
     fontFamily: 'SFProText-Bold',
     fontSize: 12,
     color: '#98A6B5',
-    lineHeight:20
-  }, 
+    lineHeight: 20
+  },
   JobText: {
     fontFamily: 'SFProText-Bold',
     fontSize: 20,
     color: '#151515',
-    marginBottom: '10%',    
-  },  
+    marginBottom: '10%',
+  },
   JobText2: {
     fontFamily: 'SFProText-Bold',
     fontSize: 15,
     color: 'white',
     marginTop: "auto",
-    fontWeight:'600',
-    paddingBottom:20
-  },  
-  JobInfo:{
+    fontWeight: '600',
+    paddingBottom: 20
+  },
+  JobInfo: {
     flex: 1,
     flexDirection: 'row',
-    paddingTop:25
+    paddingTop: 15,
   },
 })
 
